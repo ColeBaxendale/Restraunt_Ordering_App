b304070654d0357b8f238190595073e7b8cdbb9d
Cole Baxendale <cbaxendale99@gmail.com>
2024-04-17 21:56:30 -0400

    Remove unused imports from AdminShowDialogComponent
    This commit removes unused imports from the AdminShowDialogComponent file. The imports removed are `UserService`, `HttpClient`, `Router`, and their corresponding declaration files. The component no longer uses

--- frontend/src/app/components/admin-components/admin-show-dialog/admin-show-dialog.component.ts
+++ frontend/src/app/components/admin-components/admin-show-dialog/admin-show-dialog.component.ts
@@ -1,8 +1,5 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { UserService } from '../../../services/owner/user.service';
-import { HttpClient } from '@angular/common/http';
 import { MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog';
-import { Router } from 'express';
 import { User } from '../../../../../types';
 import { CommonModule, NgFor } from '@angular/common';
 
@@ -11,27 +8,20 @@ import { CommonModule, NgFor } from '@angular/common';
   standalone: true,
   imports: [CommonModule, NgFor],
   templateUrl: './admin-show-dialog.component.html',
-  styleUrl: './admin-show-dialog.component.css'
+  styleUrl: './admin-show-dialog.component.css',
 })
-export class AdminShowDialogComponent implements OnInit{
+export class AdminShowDialogComponent implements OnInit {
   constructor(
-    private userService: UserService,
     public dialog: MatDialog,
-    private http: HttpClient,
-    private router: Router,
     @Inject(MAT_DIALOG_DATA) public data: { owner: User }
   ) {}
 
-
-
   ngOnInit(): void {
     this.loadUser();
-
-    
   }
 
   loadUser(): void {
     console.log(this.data.owner.email);
     console.log(this.data.owner._id);
-}
+  }
 }
