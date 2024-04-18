0861a55a8a2925dfbb9688e86edd2a1c5ae76128
Cole Baxendale <cbaxendale99@gmail.com>
2024-04-17 21:57:21 -0400

    Update AdminAddDialogComponent: remove unnecessary imports, modify CSS, and simplify constructor
    The changes include:
    1. Remove unused import 'R3SelectorScopeMode' from '@angular/compiler'.
    2. Modify CSS styles for

--- frontend/src/app/components/admin-components/admin-add-dialog/admin-add-dialog.component.ts
+++ frontend/src/app/components/admin-components/admin-add-dialog/admin-add-dialog.component.ts
@@ -5,7 +5,6 @@ import { FormsModule } from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { UserService } from '../../../services/owner/user.service';
 import { Router } from '@angular/router';
-import { R3SelectorScopeMode } from '@angular/compiler';
 
 @Component({
   selector: 'app-admin-add-dialog',
@@ -18,7 +17,9 @@ import { R3SelectorScopeMode } from '@angular/compiler';
 export class AdminAddDialogComponent {
   constructor(
     public dialogRef: MatDialogRef<AdminAddDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any, private userService: UserService, private router: Router, ) {}
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private userService: UserService,
+  ) {}
   errorMsg = '';
   user: User = {
     email: '',
@@ -33,20 +34,20 @@ export class AdminAddDialogComponent {
     this.userService.createUser(this.user).subscribe({
       next: (response: UserResponse) => {
         console.log('Successfully created user:', response.message);
-        // Assuming response.user holds the created user data
         console.log(response.userId);
-        
+
         if (response.userId) {
-          this.dialogRef.close(response.userId);  // Pass the created user back
+          this.dialogRef.close(response.userId);
         } else {
-          this.dialogRef.close(); // Close without data if user is not in the response
+          this.dialogRef.close();
         }
-      },   
+      },
       error: (error) => {
         console.error('Failed to create user:', error);
-        this.errorMsg = error.error ? error.error.message : 'An unknown error occurred';
+        this.errorMsg = error.error
+          ? error.error.message
+          : 'An unknown error occurred';
       },
     });
   }
-  
 }
