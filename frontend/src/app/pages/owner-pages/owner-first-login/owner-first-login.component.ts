af6bd8a19acdc7e6dc811a2c961e5dcee4b60943
BasicallyClutch <97856451+ColeBaxendale@users.noreply.github.com>
2024-07-22 22:59:08 -0400

    "Updated admin component HTML to disable logout button, added CSS and HTML for owner-first-login component, and updated owner-first-login component TS to import necessary modules and add email form control."

--- frontend/src/app/pages/owner-pages/owner-first-login/owner-first-login.component.ts
+++ frontend/src/app/pages/owner-pages/owner-first-login/owner-first-login.component.ts
@@ -1,12 +1,28 @@
+import { NgIf, CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 @Component({
   selector: 'app-owner-first-login',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf,
+    FormsModule,
+    CommonModule,
+    MatInputModule,
+    MatIconModule,
+    ReactiveFormsModule,
+    MatCheckboxModule,
+    MatProgressBarModule,
+  ],
   templateUrl: './owner-first-login.component.html',
   styleUrl: './owner-first-login.component.css'
 })
 export class OwnerFirstLoginComponent {
+  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
 
 }
